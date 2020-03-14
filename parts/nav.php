d92b6457e4dcc1ab876aafcbf86f23ac82bbd4c4
Moritz Kaspar Rudert (mortzu) <me@mortzu.de>
2020-03-14 20:34:42 +0100

    Removed disabled nav item

--- parts/nav.php
+++ parts/nav.php
@@ -11,9 +11,6 @@
         <li class="nav-item">
           <a class="nav-link" href="#">Clients</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-        </li>
       </ul>
     </div>
   </nav>
