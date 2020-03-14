b9b7652fade147cc0da2dd767a4da80ddc488abf
Moritz Kaspar Rudert (mortzu) <me@mortzu.de>
2020-03-14 21:28:03 +0100

    Make navbar dynamic

--- config.defaults.php
+++ config.defaults.php
@@ -4,6 +4,9 @@ $config = array();
 $config['auth']['realm'] = '';
 $config['auth']['file'] = $script_directory . '/auth/passwdfile';
 $config['connected_since_date_format'] = 'd.m.Y H:i:s';
+$config['nav']['status'] = array('target' => '/', 'name' => 'Status');
+$config['nav']['clients'] = array('target' => '/?page=clients', 'name' => 'Clients');
 $config['status']['socket'] = '/var/lib/openvpn/server.socket';
+$config['superuser']['auth'] = '';
 
 ?>
--- parts/nav.php
+++ parts/nav.php
@@ -1,16 +1,15 @@
   <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-    <a class="navbar-brand" href="#"><b>OpenVPN</b>Admin</a>
+    <span class="navbar-brand"><span class="font-weight-bold">OpenVPN</span>Admin</span>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarCollapse">
       <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="#">Status <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Clients</a>
-        </li>
+<?php
+foreach ($config['nav'] as $nav) {
+  echo '<li class="nav-item"><a class="nav-link" href="' . $nav['target'] . '">' . $nav['name'] . '</a></li>';
+}
+?>
       </ul>
     </div>
   </nav>
