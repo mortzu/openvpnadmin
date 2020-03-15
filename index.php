b24f41112100b0d536670bd722c07edfdbdc7437
Moritz Kaspar Rudert (mortzu) <me@mortzu.de>
2020-03-15 07:05:53 +0100

    Added stylesheet

--- index.php
+++ index.php
@@ -16,6 +16,7 @@ session_start();
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
     <title></title>
     <style>main > .container { padding: 60px 15px 0; }</style>
   </head>
@@ -30,7 +31,7 @@ session_start();
 
 <?php
 
-if (!isset($_SESSION['loggedin']) || ($_SESSION['loggedin'] !== true)) {
+if (isset($_SESSION['loggedin']) && ($_SESSION['loggedin'] == true)) {
   if (!isset($_GET['page']) || $_GET['page'] == '')
     require_once $script_directory . '/parts/connected_clients.php';
   elseif ($_GET['page'] == 'clients')
